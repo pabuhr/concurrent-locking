90d1950358250b3fe5a9eb5f87362b67724f3941
Peter A. Buhr <pabuhr@uwaterloo.ca>
2021-10-22 18:46:55 -0400

    convert Peterson to use new critical section

--- Peterson.c
+++ Peterson.c
@@ -1,52 +1,56 @@
 // G. L. Peterson, Myths About the Mutual Exclusion Problem, Information Processing Letters, 1981, 12(3), Fig. 3, p. 116
 // cnt is used to prove threads do not move evenly through levels.
 
-static VTYPE *Q CALIGN, *turns CALIGN;
+static TYPE PAD1 CALIGN __attribute__(( unused ));		// protect further false sharing
+static VTYPE * Q CALIGN, * turns CALIGN;
+static TYPE PAD2 CALIGN __attribute__(( unused ));		// protect further false sharing
 
-static void *Worker( void *arg ) {
-	TYPE id = (size_t)arg + 1;					// id 0 => don't-want-in
+static void * Worker( void * arg ) {
+	TYPE id = (size_t)arg + 1;							// id 0 => don't-want-in
 	uint64_t entry;
-#ifdef FAST
-	unsigned int cnt = 0, oid = id;
-#endif // FAST
 
-//	unsigned long int cnt[N];
+	#ifdef FAST
+	unsigned int cnt = 0, oid = id;
+	#endif // FAST
 
 	for ( int r = 0; r < RUNS; r += 1 ) {
-		entry = 0;
-//		for ( int i = 1; i < N; i += 1 ) cnt[i] = 0;
-		while ( stop == 0 ) {
+		RTYPE randomThreadChecksum = 0;
+
+		for ( entry = 0; stop == 0; entry += 1 ) {
 			for ( TYPE rd = 1; rd < N; rd += 1 ) {		// entry protocol, round
 				Q[id] = rd;								// current round
 				turns[rd] = id;							// RACE
 				Fence();								// force store before more loads
 			  L: for ( int k = 1; k <= N; k += 1 ) {		// find loser
-//					if ( k != id && Q[k] == rd ) cnt[rd] += 1;
 					if ( k != id && Q[k] >= rd && turns[rd] == id ) { Pause(); goto L; }
 				} // for
 			} // for
-			CriticalSection( id );
+
+			randomThreadChecksum += CriticalSection( id );
+
 			Q[id] = 0;									// exit protocol
-#ifdef FAST
+
+			#ifdef FAST
 			id = startpoint( cnt );						// different starting point each experiment
 			cnt = cycleUp( cnt, NoStartPoints );
-#endif // FAST
-			entry += 1;
-		} // while
-#ifdef FAST
+			#endif // FAST
+		} // for
+
+		__sync_fetch_and_add( &sumOfThreadChecksums, randomThreadChecksum );
+
+		#ifdef FAST
 		id = oid;
-#endif // FAST
+		#endif // FAST
 		entries[r][id - 1] = entry;						// adjust for id + 1
 		__sync_fetch_and_add( &Arrived, 1 );
 		while ( stop != 0 ) Pause();
 		__sync_fetch_and_add( &Arrived, -1 );
-//		printf( "%d: %ld %ld %ld %ld %ld %ld %ld\n",
-//				id, cnt[1]/entries[r][id], cnt[2]/entries[r][id], cnt[3]/entries[r][id], cnt[4]/entries[r][id], cnt[5]/entries[r][id], cnt[6]/entries[r][id], cnt[7]/entries[r][id] ); // make print atomic
 	} // for
+
 	return NULL;
 } // Worker
 
-void ctor() {
+void __attribute__((noinline)) ctor() {
 	Q = Allocator( sizeof(typeof(Q[0])) * (N + 1) );
 	turns = Allocator( sizeof(typeof(turns[0])) * (N - 1 + 1) );
 	for ( int i = 1; i <= N; i += 1 ) {					// initialize shared data
@@ -54,12 +58,12 @@ void ctor() {
 	} // for
 } // ctor
 
-void dtor() {
+void __attribute__((noinline)) dtor() {
 	free( (void *)turns );
 	free( (void *)Q );
 } // dtor
 
 // Local Variables: //
 // tab-width: 4 //
-// compile-command: "gcc -Wall -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=Peterson Harness.c -lpthread -lm" //
+// compile-command: "gcc -Wall -Wextra -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=Peterson Harness.c -lpthread -lm -D`hostname` -DCFMT -DCNT=0" //
 // End: //
--- PthreadLock.c
+++ PthreadLock.c
@@ -1,34 +1,52 @@
 pthread_mutex_t lock CALIGN;
 
-static void *Worker( void *arg ) {
+static void * Worker( void * arg ) {
 	TYPE id = (size_t)arg;
 	uint64_t entry;
 
+	#ifdef FAST
+	unsigned int cnt = 0, oid = id;
+	#endif // FAST
+
 	for ( int r = 0; r < RUNS; r += 1 ) {
-		entry = 0;
-		while ( stop == 0 ) {
+		RTYPE randomThreadChecksum = 0;
+
+		for ( entry = 0; stop == 0; entry += 1 ) {
 			pthread_mutex_lock( &lock );
-			CriticalSection( id );
+
+			randomThreadChecksum += CriticalSection( id );
+
 			pthread_mutex_unlock( &lock );
-			entry += 1;
-		} // while
+
+			#ifdef FAST
+			id = startpoint( cnt );						// different starting point each experiment
+			cnt = cycleUp( cnt, NoStartPoints );
+			#endif // FAST
+		} // for
+
+		__sync_fetch_and_add( &sumOfThreadChecksums, randomThreadChecksum );
+
+		#ifdef FAST
+		id = oid;
+		#endif // FAST
 		entries[r][id] = entry;
 		__sync_fetch_and_add( &Arrived, 1 );
 		while ( stop != 0 ) Pause();
 		__sync_fetch_and_add( &Arrived, -1 );
 	} // for
+
 	return NULL;
 } // Worker
 
-void ctor() {
+void __attribute__((noinline)) ctor() {
 	pthread_mutex_init( &lock, NULL );
 } // ctor
 
-void dtor() {
+void __attribute__((noinline)) dtor() {
 	pthread_mutex_destroy( &lock );
 } // dtor
 
 // Local Variables: //
 // tab-width: 4 //
-// compile-command: "gcc -Wall -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=PthreadLock Harness.c -lpthread -lm" //
+// compile-command: "gcc -Wall -Wextra -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=PthreadLock Harness.c -lpthread -lm -D`hostname` -DCFMT -DCNT=0" //
 // End: //
