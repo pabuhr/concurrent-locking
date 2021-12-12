66617e6b34934352dcb8d926a5dbdc15724c528f
Peter A. Buhr <pabuhr@uwaterloo.ca>
2021-12-12 17:55:04 -0500

    multi-version MCS

--- MCS.c
+++ MCS.c
@@ -1,48 +1,53 @@
 // John M. Mellor-Crummey and Michael L. Scott, Algorithm for Scalable Synchronization on Shared-Memory Multiprocessors,
-// ACM Transactions on Computer Systems, 9(1), 1991, Fig. 6, p. 30
+// ACM Transactions on Computer Systems, 9(1), 1991, Fig. 5, p. 30
 
 #include <stdbool.h>
 
-typedef struct mcs_node {								// cache align node at declaration
+typedef struct mcs_node {
 	#ifndef ATOMIC
 	struct mcs_node * volatile next;
 	#else
 	_Atomic(struct mcs_node *) next;
 	#endif // ! ATOMIC
 	VTYPE spin;
-} MCS_node;
+} MCS_node CALIGN;
+
 #ifndef ATOMIC
 typedef MCS_node * MCS_lock;
 #else
 typedef _Atomic(MCS_node *) MCS_lock;
 #endif // ! ATOMIC
 
-void mcs_lock( MCS_lock * lock, MCS_node * node ) {
-	MCS_node * pred;
+inline void mcs_lock( MCS_lock * lock, MCS_node * node ) {
+	WO( Fence(); ); // 1
 	node->next = NULL;
-	// node->spin = 1;									// alternative position and remove fence below
-	WO( Fence(); );
-	pred = __atomic_exchange_n( lock, node, __ATOMIC_ACQUIRE );
-
-	if ( FASTPATH( pred != NULL ) ) {					// someone on list ?
-		node->spin = 1;									// mark as waiting
-		WO( Fence(); );
-		pred->next = node;								// add to list of waiting threads
-		while ( node->spin == 1 ) Pause();				// busy wait on my spin variable
-		WO( Fence(); );
-	} // if
+#ifndef MCS_OPT1										// default option
+	node->spin = true;									// alternative position and remove fence below
+	WO( Fence(); ); // 2
+#endif // MCS_OPT1
+	MCS_node * prev = __atomic_exchange_n( lock, node, __ATOMIC_SEQ_CST );
+  if ( SLOWPATH( prev == NULL ) ) return;				// no one on list ?
+#ifdef MCS_OPT1
+	node->spin = true;									// mark as waiting
+	WO( Fence(); ); // 3
+#endif // MCS_OPT1
+	prev->next = node;									// add to list of waiting threads
+	WO( Fence(); ); // 4
+	while ( node->spin == true ) Pause();				// busy wait on my spin variable
+	WO( Fence(); ); // 5
 } // mcs_lock
 
-void mcs_unlock( MCS_lock * lock, MCS_node * node ) {
-#if 0  // original
-	if ( node->next == NULL ) {							// no one waiting ?
+inline void mcs_unlock( MCS_lock * lock, MCS_node * node ) {
+	WO( Fence(); ); // 6
+#ifndef MCS_OPT2										// original, default option
+	if ( FASTPATH( node->next == NULL ) ) {				// no one waiting ?
 		MCS_node * temp = node;							// copy because exchange overwrites expected
   if ( __atomic_compare_exchange_n( lock, &temp, NULL, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST ) ) return; // Fence
 		while ( node->next == NULL ) Pause();			// busy wait until my node is modified
 	} // if
-	WO( Fence(); );
-	node->next->spin = 0;								// stop their busy wait
-#else  // Dice
+	WO( Fence(); ); // 7
+	node->next->spin = false;							// stop their busy wait
+#else													// Dice option
 	MCS_node * succ = node->next;
 	if ( FASTPATH( succ == NULL ) ) {					// no one waiting ?
 		// node is potentially at the tail of the MCS chain 
@@ -52,9 +57,9 @@ void mcs_unlock( MCS_lock * lock, MCS_node * node ) {
 		// as the successor has yet to store into node->next.
 		while ( (succ = node->next) == NULL ) Pause();	// busy wait until my node is modified
 	} // if
-	WO( Fence(); );
-	succ->spin = 0;
-#endif // 0
+	WO( Fence(); ); // 8
+	succ->spin = false;
+#endif // MCS_OPT2
 } // mcs_unlock
 
 static TYPE PAD1 CALIGN __attribute__(( unused ));		// protect further false sharing
@@ -69,7 +74,7 @@ static void * Worker( void * arg ) {
 	unsigned int cnt = 0, oid = id;
 	#endif // FAST
 
-	MCS_node node CALIGN;								// sufficient to cache align node
+	MCS_node node;
 
 	for ( int r = 0; r < RUNS; r += 1 ) {
 		RTYPE randomThreadChecksum = 0;
@@ -97,6 +102,7 @@ static void * Worker( void * arg ) {
 		while ( stop != 0 ) Pause();
 		__sync_fetch_and_add( &Arrived, -1 );
 	} // for
+
 	return NULL;
 } // Worker
 
