4afede23fe28cc19190a9b7ba72d174a3a0b1693
Peter A. Buhr <pabuhr@uwaterloo.ca>
2022-05-19 18:07:20 -0400

    add MCSH.c and run script for it

---
+++ MCSH.c
@@ -0,0 +1,113 @@
+#include <stdbool.h>
+
+typedef struct qnode_t {
+	#ifndef ATOMIC
+	struct qnode_t * volatile
+	#else
+	_Atomic(struct qnode_t *)
+	#endif // ! ATOMIC
+		next;
+	VTYPE go;
+} qnode CALIGN;
+
+#ifndef ATOMIC
+typedef qnode *
+#else
+typedef _Atomic(qnode *)
+#endif // ! ATOMIC
+	qnode_ptr;
+
+typedef struct {
+	qnode_ptr tail CALIGN;
+	qnode_ptr mess CALIGN;
+	VTYPE flag CALIGN;
+} NMCS_lock;
+
+static inline void mcs_lock( NMCS_lock * lock ) {
+	qnode mm = { NULL, false };
+	qnode_ptr prev = Fas( &lock->tail, &mm );
+
+	if ( SLOWPATH( prev == NULL ) ) {
+		await( lock->flag );
+	} else {
+		prev->next = &mm;
+		await( mm.go );
+	} // if
+
+	WO( Fence(); );
+	lock->flag = false;
+	WO( Fence(); );
+	qnode_ptr succ = mm.next;
+	if ( FASTPATH( succ == NULL ) ) {
+		if ( FASTPATH( ! Cas( &lock->tail, &mm, NULL ) ) ) {
+			await( (succ = mm.next) != NULL );
+		} // if
+	} // if
+	WO( Fence(); );
+	lock->mess = succ;
+} // mcs_lock
+
+static inline void mcs_unlock( NMCS_lock * lock ) {
+	qnode_ptr temp = lock->mess;
+	WO( Fence(); );
+	lock->flag = true;
+	if ( FASTPATH( temp != NULL ) ) {
+		WO( Fence(); );
+		temp->go = true;
+	} // if
+} // mcs_unlock
+
+static TYPE PAD1 CALIGN __attribute__(( unused ));		// protect further false sharing
+static NMCS_lock lock CALIGN;
+static TYPE PAD2 CALIGN __attribute__(( unused ));		// protect further false sharing
+
+static void * Worker( void * arg ) {
+	TYPE id = (size_t)arg;
+	uint64_t entry;
+
+	#ifdef FAST
+	unsigned int cnt = 0, oid = id;
+	#endif // FAST
+
+	for ( int r = 0; r < RUNS; r += 1 ) {
+		RTYPE randomThreadChecksum = 0;
+
+		for ( entry = 0; stop == 0; entry += 1 ) {
+			mcs_lock( &lock );
+
+			randomThreadChecksum += CriticalSection( id );
+
+			mcs_unlock( &lock );
+
+			#ifdef FAST
+			id = startpoint( cnt );						// different starting point each experiment
+			cnt = cycleUp( cnt, NoStartPoints );
+			#endif // FAST
+		} // for
+
+		Fai( &sumOfThreadChecksums, randomThreadChecksum );
+
+		#ifdef FAST
+		id = oid;
+		#endif // FAST
+		entries[r][id] = entry;
+		Fai( &Arrived, 1 );
+		while ( stop != 0 ) Pause();
+		Fai( &Arrived, -1 );
+	} // for
+
+	return NULL;
+} // Worker
+
+void __attribute__((noinline)) ctor() {
+	lock.tail = NULL;
+	lock.flag = true;
+} // ctor
+
+void __attribute__((noinline)) dtor() {
+} // dtor
+
+// Local Variables: //
+// tab-width: 4 //
+// compile-command: "gcc -Wall -Wextra -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=MCSH Harness.c -lpthread -lm -D`hostname` -DCFMT -DCNT=0" //
+// End: //
