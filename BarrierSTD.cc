d61b4e67da1bc49c187b3e37a213ba89896577dc
Peter A. Buhr <pabuhr@uwaterloo.ca>
2025-06-23 12:09:09 -0400

    fix emacs compile command

--- BarrierSTD.cc
+++ BarrierSTD.cc
@@ -17,5 +17,5 @@ void __attribute__((noinline)) dtor() {
 } // dtor
 
 // Local Variables: //
-// compile-command: "g++-11 -Wall -Wextra -Werror -Wno-volatile -std=c++20 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=BarrierSTD Harness.c -lpthread -lm -D`hostname` -DCFMT" //
+// compile-command: "g++-11 -Wall -Wextra -Werror -Wno-volatile -std=c++20 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DAlgorithm=BarrierSTD Harness.cc -lpthread -lm -D`hostname` -DCFMT" //
 // End: //
