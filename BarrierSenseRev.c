ac30310ff7c7eb0121d95cc326560371d1f6b1df
Peter A. Buhr <pabuhr@uwaterloo.ca>
2026-06-19 11:56:53 -0400

    change to Hensgen's sense reverse

--- BarrierSenseRev.c
+++ BarrierSenseRev.c
@@ -1,52 +1,83 @@
 // Debra Hensgen, 1 Raphael Finkel, 1 and Udi Manber, Two Algorithms for Barrier Synchronization International Journal
 // of Parallel Programming, Vol. 17, No. 1, 1988 p. 7-8
-//
-// John M. Mellor-Crummey and Michael L. Scott, Algorithms for Scalable Synchronization on Shared-Memory Multiprocessors,
-// ACM Transactions on Computer Systems, 9(1), February 1991, Fig. 8, p. 34
 
-#include "BarrierCallback.h"
+// Cannot have callback/distinguished-thread without changing from symmetric.
 
 typedef struct {
-	TYPE CALIGN group;
-	VTYPE CALIGN flag;
-	VTYPE CALIGN count;
-	pthread_mutex_t lock;
-	CBDECL();
+	TYPE LogNPROCS;
+	TYPE ** intended;
+	VTYPE ** AnswersOdd, ** AnswersEven;
 } Barrier;
 
 static TYPE PAD1 CALIGN __attribute__(( unused ));		// protect further false sharing
 static Barrier b CALIGN;
 static TYPE PAD2 CALIGN __attribute__(( unused ));		// protect further false sharing
 
+static __thread TYPE episode = 0;
+
 #define BARRIER_DECL
-#define BARRIER_CALL block( &b );
+#define BARRIER_CALL block( &b, p, &episode );
 
-static inline bool block( Barrier * b ) {
-	CBSTART();											// must be first
-	TYPE negflag = ! b->flag;							// optimization (compiler probably does it)
-	pthread_mutex_lock( &b->lock );
+static inline void block( Barrier * b, TYPE p, TYPE * episode ) {
+	TYPE OddOrEven = *episode % 2;
+	TYPE signal = (*episode / 2) % 2;
 
-	b->count += 1;
-	if ( LIKELY( b->count < b->group ) ) {
-		pthread_mutex_unlock( &b->lock );
-		await( b->flag == negflag );
-		return false;
-	} // if
-	CBEND();											// must appear in safe location
-	b->count = 0;
-	pthread_mutex_unlock( &b->lock );
-	b->flag = negflag;
-	return true;
+	for ( TYPE instance = 0; instance < b->LogNPROCS; instance += 1 ) {
+		TYPE their_idx = b->intended[p][instance];		// optimization
+		if ( OddOrEven == 0 ) {
+			b->AnswersEven[their_idx][instance] = signal;
+			Fence();
+			await( b->AnswersEven[p][instance] == signal );
+		} else {
+			b->AnswersOdd[their_idx][instance] = signal;
+			Fence();
+			await( b->AnswersOdd[p][instance] == signal );
+		} // if
+	} // for
+	*episode += 1;
 } // block
 
 #include "BarrierWorker.c"
 
 void __attribute__((noinline)) ctor() {
 	worker_ctor();
-	b = (Barrier){ .group = N, .count = 0, .flag = 0, .lock = PTHREAD_MUTEX_INITIALIZER CBINIT() };
+	TYPE LogNPROCS = Clog2( N );
+
+	b.LogNPROCS = LogNPROCS;
+	b.intended = Allocator( N * sizeof(b.intended[0]) );
+	for ( typeof(N) r = 0; r < N; r += 1 ) {
+		b.intended[r] = Allocator( LogNPROCS * sizeof(b.intended[0][0]) );
+	} // for
+
+	TYPE power = 1;
+	for ( typeof(LogNPROCS) c = 0; c < LogNPROCS; c += 1 ) {
+		for ( typeof(N) r = 0; r < N; r += 1 ) {
+			b.intended[r][c] = (power + r) % N;
+		} // for
+		power *= 2;
+	} // for
+
+	b.AnswersOdd =  Allocator( N * sizeof(b.AnswersOdd[0]) );
+	b.AnswersEven = Allocator( N * sizeof(b.AnswersEven[0]) );
+	for ( typeof(N) r = 0; r < N; r += 1 ) {
+		b.AnswersOdd[r]  = Allocator( LogNPROCS * sizeof(b.AnswersOdd[0][0]) );
+		b.AnswersEven[r] = Allocator( LogNPROCS * sizeof(b.AnswersEven[0][0]) );
+		for ( typeof(LogNPROCS) c = 0; c < LogNPROCS; c += 1 ) {
+			b.AnswersOdd[r][c] = 1;
+			b.AnswersEven[r][c] = 1;
+		} // for
+	} // for
 } // ctor
 
 void __attribute__((noinline)) dtor() {
+	for ( typeof(N) r = 0; r < N; r += 1 ) {
+		free( (void *)b.intended[r] );
+		free( (void *)b.AnswersEven[r] );
+		free( (void *)b.AnswersOdd[r] );
+	} // for
+	free( b.intended );
+	free( b.AnswersEven );
+	free( b.AnswersOdd );
 	worker_dtor();
 } // dtor
 
