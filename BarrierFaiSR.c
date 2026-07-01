1f88633d9af73204977be6f7ee59e8e51ae39c9a
Peter A. Buhr <pabuhr@uwaterloo.ca>
2026-07-01 16:42:39 -0400

    change variable names to match with MCS algorithm

--- BarrierFaiSR.c
+++ BarrierFaiSR.c
@@ -3,14 +3,12 @@
 
 // Counts up versus down and avoids integer overflow by reseting count after each barrier completion.
 
-// Integer overflow works because of equality testing. Requires group size of integer-size + 1 to fail.
-
 #include "BarrierCallback.h"
 
 typedef struct {
 	TYPE CALIGN group;
-	VTYPE CALIGN flag;
-	VTYPE CALIGN counter;
+	VTYPE CALIGN sense;
+	VTYPE CALIGN count;
 	CBDECL();
 } Barrier;
 
@@ -23,16 +21,16 @@ static TYPE PAD2 CALIGN __attribute__(( unused ));		// protect further false sha
 
 static inline bool block( Barrier * b ) {
 	CBSTART();											// must be first
-	TYPE nepoch = ! b->flag;							// optimization (compiler probably does it)
+	TYPE nepoch = ! b->sense;							// optimization (compiler probably does it)
 
-	if ( LIKELY( Fai( b->counter, 1 ) != b->group - 1 ) ) { // not leader ?
-		await( b->flag == nepoch );						// wait for quorum
+	if ( LIKELY( Fai( b->count, 1 ) != b->group - 1 ) ) { // not leader ?
+		await( b->sense == nepoch );						// wait for quorum
 		return false;
 	} // if
 	CBEND();											// must appear in safe location
-	b->counter = 0;										// reset arrival counter
+	b->count = 0;										// reset arrival counter
 	WO( Fence(); );
-	b->flag = nepoch;									// reset to next epoch
+	b->sense = nepoch;									// reset to next epoch
 	return true;
 } // block
 
@@ -40,7 +38,7 @@ static inline bool block( Barrier * b ) {
 
 void __attribute__((noinline)) ctor() {
 	worker_ctor();
-	b = (Barrier){ .group = N, .flag = false, .counter = 0 CBINIT() };
+	b = (Barrier){ .group = N, .sense = false, .count = 0 CBINIT() };
 } // ctor
 
 void __attribute__((noinline)) dtor() {
@@ -48,5 +46,5 @@ void __attribute__((noinline)) dtor() {
 } // dtor
 
 // Local Variables: //
-// compile-command: "gcc -Wall -Wextra -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DBARRIER -DAlgorithm=BarrierFai Harness.c -lpthread -lm -D`hostname` -DCFMT" //
+// compile-command: "gcc -Wall -Wextra -std=gnu11 -O3 -DNDEBUG -fno-reorder-functions -DPIN -DBARRIER -DAlgorithm=BarrierFaiSR Harness.c -lpthread -lm -D`hostname` -DCFMT" //
 // End: //
